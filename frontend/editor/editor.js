573bdabc4f4785b8e74ae2fbec56d5a7a6d2edea
Cian O'Carroll <cianocarroll06@gmail.com>
2025-02-20 12:08:40 +0000

    EDITOR: Refactor into 'state' classes.

--- frontend/editor/editor.js
+++ frontend/editor/editor.js
@@ -1,8 +1,6 @@
 const PLAYER_URL_BASE = "../player?board=";
 const DIALOG_BUTTON_OK = "OK";
 const DIALOG_BUTTON_DELETE = "Delete";
-const STATE_SQUARE_EDIT = 0;
-const STATE_SQUARE_LINK = 1;
 
 const appContainer = document.getElementById("appContainer");
 const boardDiv = document.getElementById("board");
@@ -13,37 +11,187 @@ const saveBtn = document.getElementById("saveBtn");
 const playLink = document.getElementById("playLink");
 const nameBox = document.getElementById("nameBox");
 
-const editTools = document.getElementById("editTools");
-const editAddSqBtn = document.getElementById("editAddSqBtn");
-const editLinkSqBtn = document.getElementById("editLinkSqBtn");
-const editRulesBtn = document.getElementById("editRulesBtn");
-
-const linkTools = document.getElementById("linkingTools");
-const linkBackBtn = document.getElementById("linkBackBtn");
-
-const addSqDialog = document.getElementById("addSqDialog");
-const addSqDialogForm = document.getElementById("addSqDialogForm");
-const asdfId = document.getElementById("asdfId");
-const asdfLabel = document.getElementById("asdfLabel");
-const asdfColor = document.getElementById("asdfColor");
-const asdfActionType = document.getElementById("asdfActionType");
-const asdfActionParam = document.getElementById("asdfActionParam");
-const asdfDeleteBtn = document.getElementById("asdfDeleteBtn");
-
 const fileOpenDialog = document.getElementById("openDialog");
 const fileOpenDialogForm = document.getElementById("openDialogForm");
 const fileOpenDialogFile = document.getElementById("opdfFile");
 
-const rulesDialog = document.getElementById("rulesDialog");
-const rulesDialogForm = document.getElementById("rulesDialogForm");
-const rdfDiceMin = document.getElementById("rdfDiceMin");
-const rdfDiceMax = document.getElementById("rdfDiceMax");
-
 const supportErrors = document.querySelectorAll(".supportError");
 let board = new Board("Untitled", boardDiv);
-let squaresEventAbortCon = new AbortController();
 let boardUnsavedChanges = false;
-let editorState = STATE_SQUARE_EDIT;
+let editorState;
+
+class State {
+    eventAbortCon;
+
+    enter() {
+        this.eventAbortCon = new AbortController(); // AbortControllers are one time use only.
+    }
+
+    exit() {
+        this.eventAbortCon.abort(); // Remove all event listeners attached upon entering state.
+    }
+
+    // Utility function for derived classes to re
+    attachListener(target, eventName, listener) {
+        target.addEventListener(eventName, listener.bind(this), {signal: this.eventAbortCon.signal})
+    }
+}
+
+class EditState extends State {
+    #editTools = document.getElementById("editTools");
+    #editAddSqBtn = document.getElementById("editAddSqBtn");
+    #editLinkSqBtn = document.getElementById("editLinkSqBtn");
+    #editRulesBtn = document.getElementById("editRulesBtn");
+    
+    #addSqDialog = document.getElementById("addSqDialog");
+    #addSqDialogForm = document.getElementById("addSqDialogForm");
+    #asdfId = document.getElementById("asdfId");
+    #asdfLabel = document.getElementById("asdfLabel");
+    #asdfColor = document.getElementById("asdfColor");
+    #asdfActionType = document.getElementById("asdfActionType");
+    #asdfActionParam = document.getElementById("asdfActionParam");
+    #asdfDeleteBtn = document.getElementById("asdfDeleteBtn");
+
+    #rulesDialog = document.getElementById("rulesDialog");
+    #rulesDialogForm = document.getElementById("rulesDialogForm");
+    #rdfDiceMin = document.getElementById("rdfDiceMin");
+    #rdfDiceMax = document.getElementById("rdfDiceMax");
+
+    #setupSquareElement(square) {
+        let elem = square.element;
+        elem.draggable = true;
+    
+        // Setup event handlers. All of these event handlers are closures so the square object is easily accessable within each.
+        this.attachListener(elem, "dragend", (event) => {
+            square.position.x += event.layerX;
+            square.position.y += event.layerY;
+            square.update();
+            boardUnsavedChanges = true;
+        });
+        this.attachListener(elem, "contextmenu", (event) => {
+            console.log(this);
+            // Set the form inputs.
+            this.#asdfId.value = square.id;
+            this.#asdfLabel.value = square.label;
+            this.#asdfColor.value = square.color;
+            this.#asdfActionType.value = square.action.type;
+            if (square.action.parameters.length > 0)
+                this.#asdfActionParam.value = square.action.parameters[0];
+            // Show the 'delete' button.
+            this.#asdfDeleteBtn.style.display = "inline";
+            // Reshow the add dialog.
+            this.#addSqDialog.showModal();
+        });
+    }
+
+    #onAddSquareButtonClicked(event) {
+        console.log(this);
+        this.#asdfId.value = board.squareNextId; // Set the hidden form input
+        this.#asdfDeleteBtn.style.display = "none";
+        board.squareNextId++;
+        this.#addSqDialog.showModal();
+    }
+    
+    #onAddSquareDialogClosed(event) {
+        let fd, btnName, newSq, newSqId, newSqAction, newSqPos;
+        btnName = event.target.returnValue;
+    
+        fd = new FormData(this.#addSqDialogForm);
+        newSqId = fd.get("sqId");
+        if (btnName === DIALOG_BUTTON_DELETE) { // Delete square if button for it pressed.
+            delete board.squares[newSqId];
+            board.rebuildLayout();
+            boardUnsavedChanges = true;
+            return;
+        }
+        else if (btnName !== DIALOG_BUTTON_OK) // Only handle form data when user presses 'ok' button
+            return;
+        
+        newSqAction = new Action(fd.get("sqActionType"), fd.get("sqActionParam"));
+        if (board.squares.hasOwnProperty(newSqId)) // Don't reset the position of an already existing square.
+            newSqPos = board.squares[newSqId].position;
+        else 
+            newSqPos = new Vector2(10, 10);
+        
+        newSq = new BoardSquare(board, newSqId, fd.get("sqLabel"), fd.get("sqColor"), newSqPos, newSqAction, "");
+        board.squares[newSqId] = newSq;
+        board.rebuildLayout();
+        this.#setupSquareElement(newSq);
+        boardUnsavedChanges = true;
+    }
+
+    #onRulesButtonClicked(event) {
+        // Show existing values.
+        this.#rdfDiceMin.value = board.rules.diceMin;
+        this.#rdfDiceMax.value = board.rules.diceMax;
+    
+        this.#rulesDialog.showModal();
+    }
+    
+    #onRulesDialogClosed(event) {
+        let btnName, fd;
+        btnName = event.target.returnValue;
+        if (btnName !== DIALOG_BUTTON_OK) // Only handle form data when user presses 'ok' button
+            return;
+        
+        fd = new FormData(this.#rulesDialogForm);
+        board.rules.diceMin = Number(fd.get("diceMin"));
+        board.rules.diceMax = Number(fd.get("diceMax"));
+        boardUnsavedChanges = true;
+    }
+
+    #onLinkSquareButtonClicked(event) {
+        changeState(new LinkState());
+    }
+
+    enter() {
+        // Enter the state.
+        super.enter();
+        this.attachListener(this.#editAddSqBtn, "click", this.#onAddSquareButtonClicked);
+        this.attachListener(this.#editLinkSqBtn, "click", this.#onLinkSquareButtonClicked);
+        this.attachListener(this.#editRulesBtn, "click", this.#onRulesButtonClicked);
+        this.attachListener(this.#addSqDialog, "close", this.#onAddSquareDialogClosed);
+        this.attachListener(this.#addSqDialog, "cancel", onDialogCanceled);
+        this.attachListener(this.#rulesDialog, "close", this.#onRulesDialogClosed);
+        this.attachListener(this.#rulesDialog,"cancel", onDialogCanceled);
+        this.#editTools.style.display = "block";
+        for (let sqId in board.squares) {
+            console.log(sqId);
+            let sq = board.squares[sqId];
+            this.#setupSquareElement(sq);
+        }
+    }
+
+    exit() {
+        // Cleanup the state.
+        editTools.style.display = "none";
+        for (let sqId in board.squares) {
+            let e = board.squares[sqId].element;
+            e.draggable = false;
+        }
+        super.exit();
+    }
+}
+
+class LinkState extends State {
+    #linkTools = document.getElementById("linkingTools");
+    #linkBackBtn = document.getElementById("linkBackBtn");
+
+    #onBackButtonClicked(event) {
+        changeState(new EditState());
+    }
+
+    enter() {
+        super.enter();
+        this.attachListener(this.#linkBackBtn, "click", this.#onBackButtonClicked);
+        this.#linkTools.style.display = "block";
+    }
+
+    exit() {
+        this.#linkTools.style.display = "none";
+        super.exit();
+    }
+}
 
 function newBoard() {
     board = new Board("Untitled", boardDiv);
@@ -80,72 +228,16 @@ function setupBoard() {
     boardUnsavedChanges = false;
     nameBox.value = board.name;
     playLink.href = PLAYER_URL_BASE + board.name;
-    setupSquares();
-}
-
-function setupSquares() {
-    squaresEventAbortCon = new AbortController(); // AbortControllers are one time use only.
-    for (let sqId in board.squares) {
-        console.log(sqId);
-        let sq = board.squares[sqId];
-        setupSquareElement(sq);
-    }
-}
-
-function setupSquareElement(square) {
-    let elem = square.element;
-    elem.draggable = true;
-
-    // Setup event handlers. All of these event handlers are closures so the square object is easily accessable within each.
-    elem.addEventListener("dragend", (event) => {
-        square.position.x += event.layerX;
-        square.position.y += event.layerY;
-        square.update();
-        boardUnsavedChanges = true;
-    }, {signal: squaresEventAbortCon.signal});
-    elem.addEventListener("contextmenu", (event) => {
-        // Set the form inputs.
-        asdfId.value = square.id;
-        asdfLabel.value = square.label;
-        asdfColor.value = square.color;
-        asdfActionType.value = square.action.type;
-        if (square.action.parameters.length > 0)
-            asdfActionParam.value = square.action.parameters[0];
-        // Show the 'delete' button.
-        asdfDeleteBtn.style.display = "inline";
-        // Reshow the add dialog.
-        addSqDialog.showModal();
-    }, {signal: squaresEventAbortCon.signal});
+    changeState(new EditState());
 }
 
 function changeState(newState) {
     let oldState = editorState; // Get current state.
+    
+    if (typeof oldState !== "undefined")
+        oldState.exit(); // Exit old state.
+    newState.enter(); // Enter new state.
 
-    // Exit state.
-    switch (oldState) {
-        case STATE_SQUARE_EDIT:
-            editTools.style.display = "none";
-            for (let sqId in board.squares) {
-                let e = board.squares[sqId].element;
-                e.draggable = false;
-                squaresEventAbortCon.abort();
-            }
-            break;
-        case STATE_SQUARE_LINK:
-            linkTools.style.display = "none";
-            break;
-    }
-
-    // Enter state.
-    switch (newState) {
-        case STATE_SQUARE_EDIT:
-            editTools.style.display = "block";
-            setupSquares();
-            break;
-        case STATE_SQUARE_LINK:
-            linkTools.style.display = "block";
-            break;
-    }
     editorState = newState;
 }
 
@@ -183,65 +275,6 @@ function onLoadBtnPressed(event) {
     fileOpenDialog.showModal();
 }
 
-function onAddSquareButtonClicked(event) {
-    asdfId.value = board.squareNextId; // Set the hidden form input
-    asdfDeleteBtn.style.display = "none";
-    board.squareNextId++;
-    addSqDialog.showModal();
-}
-
-function onAddSquareDialogClosed(event) {
-    let fd, btnName, newSq, newSqId, newSqAction, newSqPos;
-    btnName = event.target.returnValue;
-
-    fd = new FormData(addSqDialogForm);
-    newSqId = fd.get("sqId");
-    if (btnName === DIALOG_BUTTON_DELETE) { // Delete square if button for it pressed.
-        delete board.squares[newSqId];
-        board.rebuildLayout();
-        boardUnsavedChanges = true;
-        return;
-    }
-    else if (btnName !== DIALOG_BUTTON_OK) // Only handle form data when user presses 'ok' button
-        return;
-    
-    newSqAction = new Action(fd.get("sqActionType"), fd.get("sqActionParam"));
-    if (board.squares.hasOwnProperty(newSqId)) // Don't reset the position of an already existing square.
-        newSqPos = board.squares[newSqId].position;
-    else 
-        newSqPos = new Vector2(10, 10);
-    
-    newSq = new BoardSquare(board, newSqId, fd.get("sqLabel"), fd.get("sqColor"), newSqPos, newSqAction, "");
-    board.squares[newSqId] = newSq;
-    board.rebuildLayout();
-    setupSquareElement(newSq);
-    boardUnsavedChanges = true;
-}
-
-function onLinkSquareButtonClicked(event) {
-    changeState(STATE_SQUARE_LINK);
-}
-
-function onRulesButtonClicked(event) {
-    // Show existing values.
-    rdfDiceMin.value = board.rules.diceMin;
-    rdfDiceMax.value = board.rules.diceMax;
-
-    rulesDialog.showModal();
-}
-
-function onRulesDialogClosed(event) {
-    let btnName, fd;
-    btnName = event.target.returnValue;
-    if (btnName !== DIALOG_BUTTON_OK) // Only handle form data when user presses 'ok' button
-        return;
-    
-    fd = new FormData(rulesDialogForm);
-    board.rules.diceMin = Number(fd.get("diceMin"));
-    board.rules.diceMax = Number(fd.get("diceMax"));
-    boardUnsavedChanges = true;
-}
-
 function onFileOpenDialogClosed(event) {
     let btnName, fd, file, reader;
     btnName = event.target.returnValue;
@@ -262,23 +295,11 @@ function onDialogCanceled(event) {
     event.target.returnValue = "esc"; // Act like the cancel button was pressed upon Escape being pressed.
 }
 
-function onBackButtonClicked(event) {
-    changeState(STATE_SQUARE_EDIT);
-}
-
 window.addEventListener("load", onPageLoad);
 window.addEventListener("beforeunload", onPageUnload);
 newBtn.addEventListener("click", (ev) => newBoard());
 loadBtn.addEventListener("click", onLoadBtnPressed);
 saveBtn.addEventListener("click", (ev) => downloadBoardToFile(board.name, board.saveJson()));
 nameBox.addEventListener("change", onNameBoxChange)
-editAddSqBtn.addEventListener("click", onAddSquareButtonClicked);
-editLinkSqBtn.addEventListener("click", onLinkSquareButtonClicked);
-editRulesBtn.addEventListener("click", onRulesButtonClicked);
-linkBackBtn.addEventListener("click", onBackButtonClicked);
-addSqDialog.addEventListener("close", onAddSquareDialogClosed);
-addSqDialog.addEventListener("cancel", onDialogCanceled);
 fileOpenDialog.addEventListener("close", onFileOpenDialogClosed);
 fileOpenDialog.addEventListener("cancel", onDialogCanceled);
-rulesDialog.addEventListener("close", onRulesDialogClosed);
-rulesDialog.addEventListener("cancel", onDialogCanceled);
